cd4e20fc7de572ed06d255d629c03c3712ad0b92
rosindubh <p.welsby2@ntlworld.com>
2021-08-12 12:02:20 +0100

    correct name of function called

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*phil welsby - 11 aug 2021 - one player dice game
   Function to generate random number*/
 
-const number = document.getElementById('button');
+/*const number = document.getElementById('button');
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -9,4 +9,12 @@ function randomNumber(min, max) {
 }
 
 num = randomNumber(1, 7);
-console.dir(num);
+console.dir(num);*/
+
+function generateRandomNumber(){
+
+  const rand = Math.floor(Math.random() * (7 - 1) + 1);
+ /* const rand = Math.random();*/
+  document.getElementById('display').innerText = rand;
+  
+}
