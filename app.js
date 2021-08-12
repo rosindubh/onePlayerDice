250d5366c9bcecf6875efd20f217185b30f2d050
rosindubh <p.welsby2@ntlworld.com>
2021-08-12 12:38:12 +0100

    got dice image to match random number generated

--- app.js
+++ app.js
@@ -14,7 +14,34 @@ console.dir(num);*/
 function generateRandomNumber(){
 
   const rand = Math.floor(Math.random() * (7 - 1) + 1);
- /* const rand = Math.random();*/
   document.getElementById('display').innerText = rand;
+  if (rand == 1) {
+    img = document.querySelector('img');
+    img.src = "./images/1.png";
+  }
+  else if (rand ==2) {
+    img = document.querySelector('img');
+    img.src = "./images/2.png";
+  }
+  else if (rand ==3) {
+    img = document.querySelector('img');
+    img.src = "./images/3.png";
+  }
+  else if (rand ==4) {
+    img = document.querySelector('img');
+    img.src = "./images/4.png";
+  }
+  else if (rand ==5) {
+    img = document.querySelector('img');
+    img.src = "./images/5.png";
+  }
+  else if (rand ==6) {
+    img = document.querySelector('img');
+    img.src = "./images/6.png";
+  }
   
 }
+
+/*const img = document.querySelector("img"); 
+img.src = "./images.i.png";*/
+
