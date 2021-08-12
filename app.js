3a89cec56d4b17fc487b8e1b7c4e4660101e8899
rosindubh <p.welsby2@ntlworld.com>
2021-08-12 18:52:27 +0100

    test button generates random numbers 1 - 6

--- app.js
+++ app.js
@@ -8,33 +8,8 @@ const playerScoreboard = document.getElementById('scoreboard');
 const dice = document.getElementById('dice');
 const rollButton = document.getElementById('rollButton');
 
-/*function generateRandomNumber(){
+rollButton.addEventListener('click', () =>{
+  let diceRoll = Math.floor(Math.random() * 6) +1;
+  console.log(diceRoll);
+});
 
-  const rand = Math.floor(Math.random() * (7 - 1) + 1);
-  document.getElementById('display').innerText = rand;
-  if (rand == 1) {
-    img = document.querySelector('img');
-    img.src = "./images/loser.jpg";
-  }
-  else if (rand ==2) {
-    img = document.querySelector('img');
-    img.src = "./images/2.png";
-  }
-  else if (rand ==3) {
-    img = document.querySelector('img');
-    img.src = "./images/3.png";
-  }
-  else if (rand ==4) {
-    img = document.querySelector('img');
-    img.src = "./images/4.png";
-  }
-  else if (rand ==5) {
-    img = document.querySelector('img');
-    img.src = "./images/5.png";
-  }
-  else if (rand ==6) {
-    img = document.querySelector('img');
-    img.src = "./images/6.png";
-  }
-  
-}*/
