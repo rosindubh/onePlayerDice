ee5fdb5d6b0dbb3e21295a636134f8f341259f3b
rosindubh <p.welsby2@ntlworld.com>
2021-08-13 01:14:56 +0100

    got some of game working - needs to dice images adding

--- app.js
+++ app.js
@@ -6,22 +6,36 @@ let playerScore = 0;
 /*set variables*/
 const playerHeader = document.getElementById('playerHeader');
 const playerScoreboard = document.getElementById('scoreboard');
+const player1 = document.getElementById('player1')
 const dice = document.getElementById('dice');
 const rollButton = document.getElementById('rollButton');
+const resetButton = document.getElementById("resetButton");
 
-/*LISTEN FOR*/
+/*LISTEN FOR click*/
 rollButton.addEventListener('click', () =>{
   let diceRoll = Math.floor(Math.random() * 6) +1;
-  console.log(diceRoll); /*used to test button*/
+  console.log('diceRoll =', diceRoll); /*used to test button*/
   /*YOU LOSE*/
   if (diceRoll === 1) {
     gameOver('You Lose!') /*call function <gameOver> with variable <"You Lose">*/
   }
 
+  // RESET BUTTON
+  resetButton.addEventListener("click", () => {
+    playerScore = 0;
+    playerHeader.textContent = "";
+
+    // playerDice.textContent = "-";
+    playerScoreboard.textContent = 0;
+})
+
   /*increment players score*/
   playerScore += diceRoll;
   playerScoreboard.textContent = playerScore; /*display players score to screen as an H3 heading*/
+
 });
+
+/*use <switch> to show images*/
 
 /*GAME OVER FUNCTION*/
 function gameOver(message) { /*this is a parameter*/
